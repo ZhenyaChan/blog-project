e285fd77eeb91225253b391715cfe869772e91e8
Zane Chan <dung@Zanes-MacBook-Air.local>
2024-03-05 16:54:38 -0500

    Utilized Jinja to reuse header and footer templates, updated about, contact, index and post pages to be able navigate between them, created route to dynamically change html pages

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
-    return render_template("/index.html")
+    return render_template("index.html")
+
+
+@app.route("/<path:page>")
+def static_page(page):
+    return render_template(f"{page}.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
