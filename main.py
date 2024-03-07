933c2d7ac34ab8801c52876a8cd19ef9b98a81cd
Zane Chan <dung@Zanes-MacBook-Air.local>
2024-03-07 12:41:21 -0500

    Added url for api call, implementation for holding api response and passing result to index.html

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
 
+posts = requests.get("https://api.npoint.io/e7889fd9caee6c0e8d92").json()
+
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", all_posts=posts)
 
 
 @app.route("/<path:page>")
