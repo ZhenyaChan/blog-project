7887954720eaa7b93b05071f0e7d113de68435b6
Zane Chan <dung@Zanes-MacBook-Air.local>
2024-03-07 14:04:21 -0500

    Updated routing between pages, updated index file to dynamically display passed posts

--- main.py
+++ main.py
@@ -2,20 +2,30 @@ from flask import Flask, render_template
 import requests
 
 
+posts = requests.get("https://api.npoint.io/8eb8d38c6a6fa6dcfbf1").json()
 app = Flask(__name__)
 
-posts = requests.get("https://api.npoint.io/e7889fd9caee6c0e8d92").json()
 
-
-@app.route("/")
-def home():
+@app.route('/')
+def get_all_posts():
     return render_template("index.html", all_posts=posts)
 
 
-@app.route("/<path:page>")
-def static_page(page):
-    return render_template(f"{page}.html")
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/post")
+def post():
+    return render_template("post.html")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
